33afbcd0931350b2948196e5cbddce556dda01b2
yoshuawuyts <i@yoshuawuyts.com>
2014-02-09 01:25:31 +0100

    Add /buttons/ module

--- client/modules/views/menu/menu.js
+++ client/modules/views/menu/menu.js
@@ -4,6 +4,12 @@
  * Module dependencies
  */
 
+var learnComponent = require('./buttons/learn');
+var newComponent = require('./buttons/new');
+var searchComponent = require('./buttons/search');
+var teachComponent = require('./buttons/teach');
+var userComponent = require('./buttons/user');
+
 /**
  * View
  */
@@ -16,31 +22,11 @@ module.exports = React.createClass({
       React.DOM.menu({className: "size1of8 left"},
         React.DOM.div( {className:"size1of1 pl",  role:"logo"}, "logo"),
         React.DOM.ul( {className:"pl"},
-          React.DOM.li(null,
-            React.DOM.a( {href:"#"},
-              React.DOM.button( {className:"size1of1", role:"menu-item"}, "Yoshua Wuyts")
-            )
-          ),
-          React.DOM.li(null,
-            React.DOM.a( {href:"#"},
-              React.DOM.button( {className:"size1of1", role:"menu-item"}, "New Course")
-            )
-          ),
-          React.DOM.li(null,
-            React.DOM.a( {href:"#"},
-              React.DOM.button( {className:"size1of1", role:"menu-item"}, "Learn")
-            )
-          ),
-          React.DOM.li(null,
-            React.DOM.a( {href:"#"},
-              React.DOM.button( {className:"size1of1", role:"menu-item"}, "Teach")
-            )
-          ),
-          React.DOM.li(null,
-            React.DOM.a( {href:"#"},
-              React.DOM.button( {className:"size1of1", role:"menu-item"}, "Search")
-            )
-          ),
+          userComponent(),
+          newComponent(),
+          learnComponent(),
+          teachComponent(),
+          searchComponent(),
           React.DOM.li(null,
             React.DOM.div( {className:"size1of1 bgshade5 notification-canvas"})
           )
