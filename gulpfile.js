375bc6c64022a94958f43cab607f66b9526df657
Yoshua Wuyts <yoshuawuyts@gmail.com>
2014-02-11 22:56:30 +0100

    Fix task names, edit package names, edit copy task
    Fixes a whole bunch of problems, enables the use of 'gulp taskname' more
    easily now.

--- gulpfile.js
+++ gulpfile.js
@@ -12,7 +12,7 @@ var breakpoints = require('rework-breakpoints');
 var npm = require('rework-npm');
 var vars = require('rework-vars');
 
-gulp.task('Compile and compress modules', function () {
+gulp.task('modules', function () {
   gulp
     .src('client/modules/index.js')
     .pipe(browserify({buffer: false, debug: true}))
@@ -21,13 +21,19 @@ gulp.task('Compile and compress modules', function () {
     .pipe(gulp.dest('build/scripts/'));
 });
 
-gulp.task('Copy static assets', function () {
+gulp.task('static', function () {
   gulp.src('client/index.html').pipe(gulp.dest('build/'));
   gulp.src('client/fonts/**').pipe(gulp.dest('build/fonts'));
   gulp.src('client/images/**').pipe(gulp.dest('build/images/'));
+  gulp.src("client/vendor/backbone/backbone-min.js").pipe(gulp.dest("build/vendor"));
+  gulp.src("client/vendor/backbone/backbone-min.map").pipe(gulp.dest("build/vendor"));
+  gulp.src("client/vendor/jquery/jquery.min.js").pipe(gulp.dest("build/vendor"));
+  gulp.src("client/vendor/jquery/jquery.min.map").pipe(gulp.dest("build/vendor"));
+  gulp.src("client/vendor/lodash/dist/lodash.underscore.min.js").pipe(gulp.dest("build/vendor"));
+  gulp.src("client/vendor/react/react.min.js").pipe(gulp.dest("build/vendor"));
 });
 
-gulp.task('Compile stylesheets', function () {
+gulp.task('styles', function () {
   gulp
     .src('client/styles/index.css')
     .pipe(rework(npm(), vars(), rework.colors(), rework.extend(), breakpoints))
@@ -37,25 +43,15 @@ gulp.task('Compile stylesheets', function () {
     .pipe(gulp.dest('build/styles'));
 });
 
-gulp.task('Perform tests', function () {
+gulp.task('tests', function () {
   gulp
     .src('tests/index.js')
     .pipe(mocha({ui: 'bdd', reporter: 'dot', globals: []}));
 });
 
-gulp.task('Copy client-side JavaScript libraries', function () {
-  gulp.src("client/vendor/backbone/backbone-min.js").pipe(gulp.dest("build/js"));
-  gulp.src("client/vendor/backbone/backbone-min.map").pipe(gulp.dest("build/js"));
-  gulp.src("client/vendor/jquery/jquery.min.js").pipe(gulp.dest("build/js"));
-  gulp.src("client/vendor/jquery/jquery.min.map").pipe(gulp.dest("build/js"));
-  gulp.src("client/vendor/lodash/dist/lodash.underscore.min.js").pipe(gulp.dest("build/js"));
-  gulp.src("client/vendor/react/react.min.js").pipe(gulp.dest("build/js"));
-});
-
 gulp.task('default', [
-  'Compile and compress modules',
-  'Copy static assets',
-  'Compile stylesheets',
-  'Perform tests',
-  'Copy client-side JavaScript libraries'
+  'modules',
+  'static',
+  'styles',
+  'tests'
 ]);
